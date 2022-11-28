3b835bb4fa2dbf84fdfc69293c2fd19be0c50a96
Zubee74 <you@example.com>
2022-11-28 12:44:03 +0530

    JavaScript work

---
+++ JS_ext1.js
@@ -0,0 +1,16 @@
+ //Single line comment
+            /*MultiLine comment*/
+
+            document.write("Hello From JS Code! \n")
+            //Variable 
+            var x = 10;
+            var y = 20;
+            document.write("Addition:",x+y)
+            /*
+            //Data Types
+            string z = "I am here for the JS";
+            number a = 12;
+            boolean b = true; */
+            function firstFun(){
+                document.write("Hello I am a fuction");
+            }
